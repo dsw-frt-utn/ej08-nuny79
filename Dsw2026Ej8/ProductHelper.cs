2a3fc1a29d2b35215f624903dd1243ebe98de76c
nuny79 <natalianunyperalta@gmail.com>
2026-05-04 15:23:31 -0300

    Problema 1 resuelto

---
+++ Dsw2026Ej8/ProductHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public partial class ProductHelper
+    {
+        public string ObtenerEtiquetaProducto(long code, string description, decimal price)
+        {
+            return $"[{code}] {description} - {FormatearPrecio(price)}";
+        }
+    }
+}
---
+++ Dsw2026Ej8/ProductHelperAux.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public partial class ProductHelper
+    {
+        private string FormatearPrecio(decimal price)
+        {
+            return price.ToString("C");
+        }
+    }
+}
