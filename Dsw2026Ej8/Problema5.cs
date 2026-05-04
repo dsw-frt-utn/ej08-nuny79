e17a5e7ed90b8f386c9acba16b190c10b813a7b6
nuny79 <natalianunyperalta@gmail.com>
2026-05-04 16:36:22 -0300

    Problema 5 resuelto

---
+++ Dsw2026Ej8/Problema5.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Sale
+    {
+        protected decimal _amount;
+
+        public Sale(decimal amount)
+        {
+            _amount = amount;
+        }
+
+        public virtual decimal CalculateTotal()
+        {
+            return _amount;
+        }
+    }
+
+    public class RetailSale : Sale
+    {
+        public RetailSale(decimal amount) : base(amount) { }
+
+        public override decimal CalculateTotal()
+        {
+            return _amount;
+        }
+    }
+
+    public class WholesaleSale : Sale
+    {
+        public WholesaleSale(decimal amount) : base(amount) { }
+
+        public override decimal CalculateTotal()
+        {
+            return _amount * 0.90m;
+        }
+    }
+
+    public class Problema5
+    {
+        public decimal ObtenerImporteFinal(Sale sale)
+        {
+            return sale.CalculateTotal();
+        }
+    }
+
+}
