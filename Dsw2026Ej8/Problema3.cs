065de9542825c15146e39e82eb863b95b348766a
nuny79 <natalianunyperalta@gmail.com>
2026-05-04 16:06:50 -0300

    Problema 3 resuelto

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            // copia del entero
+            int copia = originalValue;
+            copia++;
+
+            // copia de la referencia
+            Product productoCopia = product;
+            productoCopia.UpdateDescription("Descripcion modificada");
+
+            return $"{originalValue}-{copia}-{product.GetDescription()}";
+        }
+    }
+}
---
+++ Dsw2026Ej8/Product.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Product
+    {
+        private string _description;
+
+        public Product(string description)
+        {
+            _description = description;
+        }
+
+        public string GetDescription() => _description;
+
+        public void UpdateDescription(string description)
+        {
+            _description = description;
+        }
+    }
+}
