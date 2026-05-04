ea243692e018441b6b46b12d2eb3026d23742034
nuny79 <natalianunyperalta@gmail.com>
2026-05-04 16:17:42 -0300

    Problema 4 resuelto

---
+++ Dsw2026Ej8/Problema4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema4
+    {
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            double suma = 0;
+            int cantidad = 0;
+
+            foreach (int? nota in new[] { nota1, nota2, nota3 })
+            {
+                if (nota.HasValue && nota.Value >= 0 && nota.Value <= 10)
+                {
+                    suma += nota.Value;
+                    cantidad++;
+                }
+            }
+
+            return cantidad == 0 ? 0 : suma / cantidad;
+        }
+    }
+}
